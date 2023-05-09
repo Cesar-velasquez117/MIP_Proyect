0e9240fe1ccb274719b307d50ebbc95ea522d1aa
Cesar-velasquez117 <106538584+Cesar-velasquez117@users.noreply.github.com>
2023-05-08 21:43:51 -0500

    GUI for the Pre-processing and Noise Reduction Methods

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import nibabel as nib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from Algorithms.methods import k_form, region_form, thresholding_form
-from utils.helper import add_sidebar, on_validate_int, browse_file, processing, on_closing
+from utils.helper import add_sidebar, on_validate_int, browse_file, on_closing
 
 #FUNCTIONS
 
@@ -138,30 +138,31 @@ welcome_label = ctk.CTkLabel(welcome_frame, text="Welcome to this image processi
 selected_file_label = ctk.CTkLabel(welcome_frame, text="")
 files_label = ctk.CTkLabel(img_option_frame, text="Files Loaded: ", font=("Times New Roman", 20, "bold"))
 axis_label = ctk.CTkLabel(img_option_frame, text="Axis: ", font=("Times New Roman", 20, "bold"))
-segmentation_label = ctk.CTkLabel(master=radiobutton_frame, text="Choose your segmentation method: ", font=("Times New Roman", 16, "bold"))
+segmentation_label = ctk.CTkLabel(master=radiobutton_frame, text="Choose your segmentation method: ", font=("Times New Roman", 20, "bold"))
 
 #Buttons
-browse_button = ctk.CTkButton(welcome_frame, text="Browse Image", fg_color="red", width=40, height=28, command=lambda: browse_file(selected_file_label, path_list, paths_combobox))
+browse_button = ctk.CTkButton(welcome_frame, text="Browse Image", fg_color="red", width=40, height=28, command=lambda: browse_file(selected_file_label, path_list, paths_combobox, paths_combobox2))
 show_img_button = ctk.CTkButton(img_option_frame, text="Show Image", width=40 ,command= display_image)
 apply_button = ctk.CTkButton(master=radiobutton_frame, text="Apply Method", width=40,command = option_clicked)
 
 #Radio Buttons
 
 thresholding_button = ctk.CTkRadioButton(master=radiobutton_frame, text="Thresholding", variable = option, value="thresholding")
-thresholding_button.configure(font=("Times New Roman", 16))
+thresholding_button.configure(font=("Times New Roman", 20))
 reg_growing_button = ctk.CTkRadioButton(master=radiobutton_frame, text="Region Growing", variable = option, value="region growing")
-reg_growing_button.configure(font=("Times New Roman", 16))
+reg_growing_button.configure(font=("Times New Roman", 20))
 k_means_button = ctk.CTkRadioButton(master=radiobutton_frame, text="K-means", variable = option, value="k-means")
-k_means_button.configure(font=("Times New Roman", 16))
+k_means_button.configure(font=("Times New Roman", 20))
 
 #Canvas
 canvas = tkinter.Canvas(window, width=window_width/4, height=window_height/4)
 
 #Combo Box
 path_list = []
-paths_combobox = ctk.CTkComboBox(img_option_frame, width=200, height=5, state="readonly")
+image_list=[]
+paths_combobox = ctk.CTkComboBox(img_option_frame, width=200, state="readonly")
 axis_list = ["x","y","z"]
-axis_combobox = ctk.CTkComboBox(img_option_frame, width=200, height=5, values=axis_list, state="readonly")
+axis_combobox = ctk.CTkComboBox(img_option_frame, width=200, values=axis_list, state="readonly")
 
 #Pack
 #Welcome Frame
@@ -175,8 +176,8 @@ canvas.place(relx=0.5, rely=0.2, anchor="nw")
 img_option_frame.place(relx=0.25, rely=0.2, anchor="n")
 files_label.place(relx=0.05, y=20, anchor="w")
 paths_combobox.place(relx=0.35, y=20, anchor="w")
-axis_label.place(relx=0.05, y=50, anchor="w")
-axis_combobox.place(relx=0.35, y=50, anchor="w")
+axis_label.place(relx=0.05, y=60, anchor="w")
+axis_combobox.place(relx=0.35, y=60, anchor="w")
 show_img_button.place(relx=0.5, y = 180, anchor="c")
 #Processing Methods Frame
 radiobutton_frame.place(relx=0.25, rely=0.75, anchor="c")
@@ -190,20 +191,62 @@ apply_button.place(x=160, y = 200, anchor="n")
 window2 = ctk.CTkToplevel()
 window2.title("Prepocessing")
 window2.protocol("WM_DELETE_WINDOW", lambda: on_closing(window))
-#Set window dimensions
-window_width2 = int(screen_width * 0.5)
-window_height2 = int(screen_height*0.6)
-#Position
-x = int(screen_width*0.25)
-y = int(screen_height*0.25)
-window2.geometry(f"{window_width2}x{window_height2}+{x}+{y}")
+window2.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-#Button
-processing_button = ctk.CTkButton(window2, text="Go to Processing options", width=140, height=30, command=lambda: processing(window, window2))
+#Frames
+browse_frame = ctk.CTkFrame(window2, width=window_width/2, height=120)
+select_frame = ctk.CTkFrame(window2, width= window_width/3, height=110)
+standarization_frame = ctk.CTkFrame(window2, width= window_width/7, height=300)
+noise_frame= ctk.CTkFrame(window2, width= window_width/7, height=300)
+
+#Labels
+browse_label = ctk.CTkLabel(browse_frame, text="Select a file you would like to Pre-process", font=("Times New Roman", 25, "bold"))
+note_label = ctk.CTkLabel(browse_frame, text="Note that the image uploaded here will also appear in the Processing window", font=("Times New Roman", 12, "bold"))
+selected_file_label2 = ctk.CTkLabel(browse_frame, text="")
+choose_file_label=ctk.CTkLabel(select_frame, text="Choose the file you would like to Pre-process\nfrom the options below:", font=("Times New Roman", 20, "bold"))
+standarization_label = ctk.CTkLabel(standarization_frame, text="Apply standarization\nMethod", font=("Times New Roman", 16, "bold"))
+noise_label = ctk.CTkLabel(noise_frame, text="Apply noise reduction", font=("Times New Roman", 16, "bold"))
+
+standarization_option = ctk.StringVar()
+noise_option = ctk.StringVar()
+#Buttons
+browse_button = ctk.CTkButton(browse_frame, text="Browse Image", width=40, height=28, command=lambda: browse_file(selected_file_label2, path_list, paths_combobox, paths_combobox2))
+
+#Radio Buttons
+rescaling_button = ctk.CTkRadioButton(standarization_frame, text="Rescaling", variable = standarization_option, value="rescaling", font=("Times New Roman", 16))
+z_score_button = ctk.CTkRadioButton(standarization_frame, text="Z-Score", variable= standarization_option, value="z-score", font=("Times New Roman", 16))
+white_stripe_button = ctk.CTkRadioButton(standarization_frame, text="White Stripe", variable=standarization_option, value="white-stripe", font=("Times New Roman",16))
+pair_button = ctk.CTkRadioButton(standarization_frame, text="Histogram matching", variable=standarization_option,value="Histogram Matching", font=("Times New Roman", 16))
+mean_button = ctk.CTkRadioButton(noise_frame, text="Mean Filter", variable=noise_option, value="mean-filter", font=("Times New Roman", 16))
+median_button = ctk.CTkRadioButton(noise_frame, text="Median Filter", variable=noise_option, value="median-filter", font=("Times New Roman", 16))
+#ComboBox
+paths_combobox2 = ctk.CTkComboBox(select_frame, width=200, state="readonly")
+
 #Pack
-processing_button.place(relx=0.5, rely=0.5, anchor="c")
+#Browse-Frame
+browse_frame.place(relx=0.5, rely=0.01, anchor="n")
+browse_label.place(relx=0.5 , y=5,  anchor="n")
+note_label.place(relx=0.5, y=35, anchor="n")
+browse_button.place(relx=0.5, y=80, anchor="c")
+selected_file_label2.place(relx=0.5, y=90,  anchor="n")
+#Select-Frame
+select_frame.place(relx=0.75, rely=0.3, anchor="c")
+choose_file_label.place(relx=0.1, rely=0.25, anchor="w")
+paths_combobox2.place(relx=0.5, rely=0.7, anchor="c")
+#Standarization-Frame
+standarization_frame.place(relx=0.66, rely=0.45, anchor="n")
+standarization_label.place(relx=0.5, rely=0.025, anchor="n")
+rescaling_button.place(relx=0.1, rely= 0.2, anchor="w")
+z_score_button.place(relx=0.1, rely= 0.35, anchor="w")
+white_stripe_button.place(relx=0.1, rely= 0.5, anchor="w")
+pair_button.place(relx=0.1, rely= 0.65, anchor="w")
+#Noise-Frame
+noise_frame.place(relx=0.84, rely=0.45, anchor="n")
+noise_label.place(relx=0.5, rely=0.05, anchor="n")
+mean_button.place(relx=0.1, rely=0.2, anchor="w")
+median_button.place(relx=0.1, rely= 0.35, anchor="w")
 
-window.withdraw()
+window2.withdraw()
 
 #Sidebar for window
 add_sidebar(window, window2)
--- utils/helper.py
+++ utils/helper.py
@@ -7,14 +7,17 @@ from Classes.SlidePanel import SlidePanel
 def add_sidebar(window, window2):
     sidebar = SlidePanel(window, 0, -0.2)
     if window.title() == "Prepocessing": 
-        preprocess_button =  ctk.CTkButton(sidebar, text="Go back to Preprocessing", width=140, height=30, command=lambda: show_window(window2,window), state="disabled")
+        preprocess_button =  ctk.CTkButton(sidebar, text="Preprocessing Methods", width=140, height=30, command=lambda: show_window(window2,window), state="disabled")
+        processing_button = ctk.CTkButton(sidebar, text="Processing Methods", width=140, height=30, command=lambda: processing(window2, window))
     else:
-        preprocess_button =  ctk.CTkButton(sidebar, text="Go back to Preprocessing", width=140, height=30, command=lambda: show_window(window,window2))
+        preprocess_button =  ctk.CTkButton(sidebar, text="Go to Preprocessing", width=140, height=30, command=lambda: show_window(window,window2))
+        processing_button = ctk.CTkButton(sidebar, text="Processing Methods", width=140, height=30, command=lambda: processing(window, window2), state="disabled")
     sidebar_img = ctk.CTkImage(light_image=Image.open("Image/sidebar.png"),dark_image=Image.open("Image/sidebar.png"),size=(50,50))
     sidebar_button = ctk.CTkButton(window, width=50,text="", image=sidebar_img, command=sidebar.animate, fg_color="transparent", hover_color="lightgray", anchor="w")
     #Sidebar elements
     sidebar_button.place(relx=0,rely=0, anchor="nw")
-    preprocess_button.place(relx=0.5, rely=0.1, anchor="c")
+    processing_button.place(relx=0.5, rely=0.1, anchor="c")
+    preprocess_button.place(relx=0.5, rely=0.2, anchor="c")
 
 def is_int(s):
     try:
@@ -41,13 +44,17 @@ def on_validate_float(new_value):
     return is_float(new_value)
 
 #Function to browse the image_file
-def browse_file(label, list, combobox):
+def browse_file(label, list, combobox, combobox2):
     file_path = filedialog.askopenfilename()
     if file_path:
-      label.configure(text=file_path)
-      list.append(file_path)
-      #Updates the values in the Combo box
-      combobox.configure(values=list)
+      if file_path not in list:
+          label.configure(text=file_path)
+          list.append(file_path)
+          #Updates the values in the Combo box
+          combobox.configure(values=list)
+          combobox2.configure(values=list)
+      else:
+          label.configure(text="This file was already uploaded")
 
 #Function to go back to the Preprocessing window
 def show_window(window,window2):
